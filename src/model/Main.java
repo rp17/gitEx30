57b4c68aff8923bd9762920adbeb7d8f27ab109f
rp17 <rp31@txstate.edu>
2023-02-28 17:52:06 -0600

    init commit

---
+++ src/model/Main.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Main starting");
+		AgentList agList = new AgentList();
+		agList.print();
+		SensorList sensList = new SensorList();
+		sensList.print();
+	}
+
+}
